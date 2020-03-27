d8a3e03c16afc19c9bdcf12efb596a257101aa49
unknown <tridung2721@gmail.com>
2020-03-27 19:02:29 +0700

    bai 2 week3

---
+++ week3/bai2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using baitap;
+namespace baitap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TamThucBacHai f1 = new TamThucBacHai(1,-3,2);
+            TamThucBacHai f2 = new TamThucBacHai(142, -245, 100);
+            f1 = -f1;
+            f2 = -f2;
+            Console.Write("tam thuc 1 :");
+            f1.Fx();
+            Console.Write("tam thuc 2 :");
+            f2.Fx();
+            TamThucBacHai tong = f1 + f2;
+            TamThucBacHai Hieu = f1 - f2;
+            Console.Write("Tong = ");
+            tong.Fx();
+            Console.Write("Hieu = ");
+            Hieu.Fx();
+        }
+        
+    }
+}
---
+++ week3/bai2/TamThucBacHai.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baitap
+{
+    class TamThucBacHai
+    {
+        Double a, b, c;
+        public TamThucBacHai() { }
+        public TamThucBacHai(Double a, Double b, Double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void Fx()
+        {
+            char daub, dauc;
+            daub = (b <= 0) ? ' ' : '+';
+            dauc = (c <= 0) ? ' ' : '+';
+            Console.WriteLine("F(x) = {0}*x^2 {3}{1}*x {4}{2}",a,b,c,daub,dauc);
+        }
+        public static TamThucBacHai operator -(TamThucBacHai f)
+        {
+            TamThucBacHai temp = new TamThucBacHai
+            {
+                a = -f.a,
+                b = -f.b,
+                c = -f.c,
+            };
+            return temp;
+        }
+        public static TamThucBacHai operator +(TamThucBacHai f1,TamThucBacHai f2)
+        {
+            TamThucBacHai temp = new TamThucBacHai
+            {
+                a = f1.a + f2.a,
+                b = f1.b + f2.b,
+                c = f1.c + f2.c,
+            };
+            return temp;
+        }
+        public static TamThucBacHai operator -(TamThucBacHai f1, TamThucBacHai f2)
+        {
+            TamThucBacHai temp = new TamThucBacHai
+            {
+                a = f1.a - f2.a,
+                b = f1.b - f2.b,
+                c = f1.c - f2.c,
+            };
+            return temp;
+        }
+    }
+}
