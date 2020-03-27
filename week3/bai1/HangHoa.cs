3776b49a473063d35fc9827c95bcf467ea92040b
unknown <tridung2721@gmail.com>
2020-03-27 17:47:18 +0700

    bài 1 week3

---
+++ week3/bai1/HangHoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baitap
+{
+    public class HangHoa
+    {
+        string MaHang, TenHang;
+        double DonGia;
+        int SoLuong;
+
+        public void Nhap()
+        {
+            Console.Write("Nhap Ma hang : ");
+            MaHang = Console.ReadLine();
+            Console.Write("Nhap Ten Hang : ");
+            TenHang = Console.ReadLine();
+            Console.Write("Nhap Don Gia : ");
+            DonGia = int.Parse(Console.ReadLine());
+            Console.Write("Nhap So luong : ");
+            SoLuong = int.Parse(Console.ReadLine());
+        }
+        public double ThanhTien()
+        {
+            return SoLuong * DonGia;
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("\t\t{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}" ,MaHang,TenHang,DonGia,SoLuong,ThanhTien());
+        }
+    }
+}
---
+++ week3/bai1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using baitap;
+namespace baitap
+{
+    class Program
+    {
+        static void NhapHH(int n, HangHoa[] hh)
+        {
+            Console.WriteLine("\t\t Nhap Danh Sach Hang Hoa");
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\tNhap hang hoa thu {0}",i+1);
+                hh[i] = new HangHoa();
+                hh[i].Nhap();
+            }
+        }
+        static void XuatHH(int n, HangHoa[] hh)
+        {
+            Console.WriteLine("\t\tDanh Sach Hang Hoa");
+            Console.WriteLine("\t\t{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}","MaHang", "TenHang", "DonGia", "SoLuong", "ThanhTien");
+            for (int i = 0; i < n; i++)
+            {
+                hh[i].Xuat();
+            }
+        }
+        static void SapXep(int n,HangHoa [] hh)
+        {
+            for (int i = 0; i< n-1; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    if (hh[j - 1].ThanhTien() > hh[j].ThanhTien())
+                    {
+                        HangHoa t = hh[j - 1];
+                        hh[j - 1] = hh[j];
+                        hh[j] = t;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap n hang hoa : ");
+            n = int.Parse(Console.ReadLine());
+            HangHoa[] hh = new HangHoa[n];
+            NhapHH(n, hh);
+            SapXep(n, hh);
+            XuatHH(n, hh);
+            Console.ReadKey();
+        }
+        
+    }
+}
